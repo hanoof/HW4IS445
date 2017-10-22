151be3dc64ecdac2e3c71f4b5d99fa4dc467ad8c
Hanoof Alsuwailem <alhanoofsu@gmail.com>
2017-10-22 01:43:28 -0700

    commit
    commit

--- website/hw4test.js
+++ website/hw4test.js
@@ -7,10 +7,10 @@
             
             //if the input is not valid
             if (number < 2 || number > 10) {
-                //tell the user invalid number
+            
                 document.write('<h2 style="color:red">Your input is  ' + number + '. The valid input is between 2 and 10. Please reload this page and try again</h2>');
-            } else { //otherwise it is
-                //print the user's number
+            } else { 
+               
                 document.write('<h2>Your input is' + number + '</h2>');
   
                 //continue
@@ -22,11 +22,11 @@
                     numTimes++;
                 }
                 
-                //now print results
+              
                 document.write("<h1>The number of times to divide the number " + parseInt(number) + " by 2 to get a value less than one millionth is " + parseInt(numTimes) + "</h1>");
-             document.write("<br /><br /><br />"); //so the stars are below this output
+             document.write("<br /><br /><br />"); 
             
-                //the outer loop is for the rows (lines)
+               
                 for (var i=number; i>=1; i--) {
                     //inner loop is for the columns (the stars on the row (line))
                     for (var j=0; j<i; j++) {
